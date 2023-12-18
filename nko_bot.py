42ddd6b2f3f376f0fe3b50c6263a06cf37a5fede
Мария Баранова <baranova.m.p@mail.ru>
2023-12-18 18:40:12 +0500

    added inlines

--- nko_bot.py
+++ nko_bot.py
@@ -1,9 +1,9 @@
-# для проверки работоспособности запустим эхо-бот
+# для проверки инлайнов
 
 
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.filters import CommandStart
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 import os
 from dotenv import load_dotenv
 
@@ -14,27 +14,36 @@ BOT_TOKEN = os.getenv('TELEGRAM_TOKEN')
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+# Создаем объекты инлайн-кнопок
+url_button_1 = InlineKeyboardButton(
+    text='Вот здесь какая-то информация',
+    url='https://i.pinimg.com/564x/db/9a/63/db9a6371b5eec52c84d031f291a2dd50.jpg'
+)
+url_button_2 = InlineKeyboardButton(
+    text='И здесь тоже',
+    url='https://i.pinimg.com/564x/4e/16/d2/4e16d2cf75397346dd9c0850dcab67c1.jpg'
+)
+url_button_3 = InlineKeyboardButton(
+    text='Тестим кнопочки',
+    url='https://i.pinimg.com/564x/2b/3b/5e/2b3b5ea7485898de00a588bac15e03ad.jpg'
+)
+
+# Создаем объект инлайн-клавиатуры
+keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[[url_button_1],
+                     [url_button_2],
+                     [url_button_3]]
+)
+
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
+# и отправлять в чат клавиатуру c url-кнопками
+@dp.message(CommandStart())
 async def process_start_command(message: Message):
-    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
-
-
-# Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
-async def process_help_command(message: Message):
     await message.answer(
-        'Напиши мне что-нибудь и в ответ '
-        'я пришлю тебе твое сообщение'
+        text='Это инлайн-кнопки с параметром "url"',
+        reply_markup=keyboard
     )
-
-
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
-# кроме команд "/start" и "/help"
-@dp.message()
-async def send_echo(message: Message):
-    await message.reply(text=message.text)
 
 
 if __name__ == '__main__':
