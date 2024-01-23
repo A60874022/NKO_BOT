f4ec1c580af6772faf36c3c678df97b77f01925c
Anton <A60874022@yandex.ru>
2024-01-23 19:29:36 +0500

    create_index_pr

--- handlers.py
+++ handlers.py
@@ -1,67 +1,70 @@
 from aiogram import F, Router
 from aiogram.types import CallbackQuery, Message
 from keyboards import information_keyboard
-from test_data import *
+from test_data import books_data
 from motor.motor_asyncio import AsyncIOMotorClient
+
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import FSInputFile
+from word_cloud import word_cloud
+
+
 client = AsyncIOMotorClient('mongodb://localhost:27017')
 collection = client.MongoDB.MongoCollection
 router = Router()
 user_router = Router()
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
-from word_cloud import word_cloud
 storage = MemoryStorage()
+
+
 class Data(StatesGroup):
     """Машина состояний для реализации сценариев диалогов с пользователем."""
-
     information = State()
-   
+
 
 @user_router.callback_query(F.data == "information",)
 async def get_information(callback: CallbackQuery):
-    """Получение информации"""
+    """Получение информации."""
     await callback.message.answer(
-        text="Выбирайте тему для получения запроса", reply_markup=information_keyboard()
+        text="Выбирайте тему для получения запроса",
+        reply_markup=information_keyboard()
     )
 
-from aiogram import types
-from aiogram.types import FSInputFile
 
 @user_router.callback_query(F.data == "1",)
 async def get_one_information(callback: CallbackQuery, state: FSMContext):
-    """Вывод запрашиваемой информации """
+    """Вывод запрашиваемой информации."""
     await state.set_state(Data.information)
     await callback.message.answer("Введите, что вы хотите найти")
-  
+
 
 @user_router.message(Data.information)
 async def process_add_task_name(message: Message, state: FSMContext, bot):
     """Вывод запрашиваемой информации."""
-    z = await run(message.text)
+    z = await search_information(message.text)
     s = " ".join(z)
     file = await word_cloud(s)
     await bot.send_photo(message.chat.id, FSInputFile(file))
     await message.answer(
-        text=f"{z}", 
+        text=f"{z}",
     )
     await state.clear()
 
       
-async def run(name):
+async def search_information(name):
+    """Функция поиска информации в БД"""
     a = []
-    collection.create_index( { 'name': "text"} )
-    x = collection.find( { "$text": { "$search": f"{name}" } })
+    collection.create_index({'name': "text"})
+    x = collection.find({"$text": {"$search": f"{name}"}})
     async for document in x:
         a.append(document['name'])
     return (*a,)
-  
-  
+
+
 @user_router.callback_query(F.data == "Inserting information",)
 async def inserting_information(callback: CallbackQuery):
-    """Получение информации"""
-    print(500)
+    """Функция добавления информации в БД."""
     collection.insert_many(books_data)
-    print(464646)
     await callback.message.answer(
         text="Информация добавлена в БД")
--- word_cloud.py
+++ word_cloud.py
@@ -1,20 +1,24 @@
-import numpy as np
-import matplotlib.pyplot as plt
-# загружаем класс WordCloud из библиотеки wordcloud
-from wordcloud import WordCloud
-from wordcloud import STOPWORDS
 import string 
+import numpy as np
+from wordcloud import WordCloud, ImageColorGenerator
 from PIL import Image
+from collections import Counter
+
+
 async def word_cloud(text):
+    """Функция построения облаков слов"""
     text = text.lower()
     table = str.maketrans("", "", string.punctuation)
     text = text.translate(table)
+    word_counts = Counter(text.split(" ")).keys()
+    d = " ".join(word_counts)
     cake_mask = np.array(Image.open('Plan.png'))
-    wc = WordCloud(stopwords = STOPWORDS, repeat=True, 
-                   mask=cake_mask,
-                   width = cake_mask.shape[1],
-                   height = cake_mask.shape[1]).generate(text)
+    wc = WordCloud(mask=cake_mask,
+                   max_words=10).generate(d)
+    image_colors = ImageColorGenerator(cake_mask)
+    wc.recolor(color_func=image_colors)
     file = "60874022.jpg"
     wc.to_file("60874022.jpg")
-    return file 
+    return file
+
  
