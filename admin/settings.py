4e6e63a5a33cc0185d4f3487949c68f2626fa946
Anton <A60874022@yandex.ru>
2024-05-12 19:05:22 +0500

    10

--- admin/settings.py
+++ admin/settings.py
@@ -6,6 +6,6 @@ load_dotenv()
 SECRET = os.getenv("SECRET")
 ADMIN = os.getenv("ADMIN")
 PASS = os.getenv("PASS")
-MONGO_HOST = os.environ.get("MONGO_HOST", "localhost")
-MONGO_PORT = os.environ.get("MONGO_PORT", 27017)
+MONGO_HOST = os.environ.get("MONGO_HOST")
+MONGO_PORT = os.environ.get("MONGO_PORT")
 MONGO_URL = f"mongodb://{MONGO_HOST}:{MONGO_PORT}/"
