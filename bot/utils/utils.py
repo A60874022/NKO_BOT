86ff0453afea1d59abb80602a47fbb49fd645ee7
Anton <A60874022@yandex.ru>
2024-05-15 20:04:42 +0500

    10

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-MONGO_HOST = "5.35.88.241"#os.getenv("MONGO_HOST", "localhost")
-MONGO_PORT = "27017"#os.getenv("MONGO_PORT", 27017)
+MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
+MONGO_PORT = os.getenv("MONGO_PORT", 27017)
 
 
 MONGO_URL = f"mongodb://{MONGO_HOST}:{MONGO_PORT}"
