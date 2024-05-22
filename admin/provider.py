42c57f8b6f2260b5c67ea3a3bfc720d6261cdb27
Anton <A60874022@yandex.ru>
2024-05-22 18:28:21 +0500

    241

--- admin/provider.py
+++ admin/provider.py
@@ -17,11 +17,11 @@ class MyAuthProvider(AuthProvider):
         request: Request,
         response: Response,
     ) -> Response:
-        '''user = User.objects(name=username)
+        user = User.objects(name=username)
         if pwd_context.verify(password, user[0].password):
-            request.session.update({"username": username})'''
-        return response
-        #raise LoginFailed(f"{}Invalid username or password")
+            request.session.update({"username": username})
+            return response
+        raise LoginFailed(f"{username, password} - {user[0].password}")
 
     async def is_authenticated(self, request) -> bool:
         if User.objects(name=request.session.get("username", None)):
